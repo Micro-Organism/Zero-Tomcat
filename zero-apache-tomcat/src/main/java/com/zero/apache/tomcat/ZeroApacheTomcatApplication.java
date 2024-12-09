3d8fe3288b28e40526f373df2bd2765ff6379cc9
Micro-Organism <coolrabbit1993@gmail.com>
2024-12-09 19:23:44 +0800

    feat: springboot 集成 tomcat

---
+++ zero-apache-tomcat/src/main/java/com/zero/apache/tomcat/ZeroApacheTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.zero.apache.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroApacheTomcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroApacheTomcatApplication.class, args);
+    }
+
+}
---
+++ zero-apache-tomcat/src/test/java/com/zero/apache/tomcat/ZeroApacheTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.apache.tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroApacheTomcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
